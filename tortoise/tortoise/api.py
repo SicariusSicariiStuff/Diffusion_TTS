8a011f1c56f149d2c67cd138bc0ddb78cc0ce38d
SicariusSicariiStuff <144873641+SicariusSicariiStuff@users.noreply.github.com>
2023-09-20 05:07:32 +0300

    Update api.py
    to fix empty tensor for some cards

--- tortoise/tortoise/api.py
+++ tortoise/tortoise/api.py
@@ -342,7 +342,7 @@ class TextToSpeech:
                     'cond_free_k': 0, 'diffusion_temperature': 1.0}
         # Presets are defined here.
         presets = {
-            'ultra_fast': {'num_autoregressive_samples': 1, 'diffusion_iterations': 10, 'cond_free': True},
+            'ultra_fast': {'num_autoregressive_samples': 16, 'diffusion_iterations': 10, 'cond_free': True},
             'fast': {'num_autoregressive_samples': 96, 'diffusion_iterations': 80},
             'standard': {'num_autoregressive_samples': 256, 'diffusion_iterations': 200},
             'high_quality': {'num_autoregressive_samples': 256, 'diffusion_iterations': 400},
